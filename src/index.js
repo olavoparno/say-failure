4493a3d354ca8771a9e885a55bbdb22ee8910eb0
Olavo Parno <olavoparno@gmail.com>
2022-03-25 09:50:22 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-const { exec } = require('child_process')
-const { promisify } = require('util')
-const path = require('path')
-const fs = require('fs')
+const { exec } = require("child_process");
+const { promisify } = require("util");
+const path = require("path");
+const fs = require("fs");
 
-const execPromise = promisify(exec)
+const execPromise = promisify(exec);
 
-const mainPath = path.dirname(fs.realpathSync(__filename))
-const soundPath = path.join(mainPath, './audios/wololo')
-const windowsScript = path.join(mainPath, './forWindows.jscript')
+const mainPath = path.dirname(fs.realpathSync(__filename));
+const windowsScript = path.join(mainPath, "./forWindows.jscript");
+const soundConstant = "failure";
 
-const sayWololo = () => {
-    const commandsForEachPlatform = {
-      linux: `paplay ${soundPath}.ogg`,
-      win32: `cscript /E:JScript /nologo "${windowsScript}" "${soundPath}.mp3"`,
-      darwin: `afplay ${soundPath}.mp3`,
-    }
-
-    const platform = process.platform
-    const codeToExecute = commandsForEachPlatform[platform]
-
-    return execPromise(codeToExecute)
+function getSoundPath() {
+  return path.join(
+    mainPath,
+    `./audios/${soundConstant}${Math.floor(Math.random() * (7 - 1 + 1)) + 1}`
+  );
 }
 
-module.exports = sayWololo
+const sayFailure = () => {
+  const soundPath = getSoundPath();
+
+  const commandsForEachPlatform = {
+    linux: `paplay ${soundPath}.ogg`,
+    win32: `cscript /E:JScript /nologo "${windowsScript}" "${soundPath}.mp3"`,
+    darwin: `afplay ${soundPath}.mp3`,
+  };
+
+  const platform = process.platform;
+  const codeToExecute = commandsForEachPlatform[platform];
+
+  return execPromise(codeToExecute);
+};
+
+module.exports = sayFailure;
